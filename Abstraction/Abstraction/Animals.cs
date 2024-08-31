1b8523591570ae828c46f0be80067167a26c199c
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-31 15:48:49 +0400

    Abstraction created

---
+++ Abstraction/Abstraction/Animals.cs
@@ -0,0 +1,21 @@
+﻿public abstract class Animal
+{
+    public abstract void MakeSound(); // Abstract method
+
+    public void Sleep() // Concrete method
+    {
+        Console.WriteLine("Sleeping...");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woof Woof!");
+    }
+}
+
+
+//Abstraction, yalnız müvafiq məlumatları təqdim etməyə və funksionallığı istifadəçidən gizlətməyə imkan verir. Siniflər və interfeyslər bu prinsipi tətbiq etmək üçün istifadə olunur. Abstraction proqramlaşdırıcının yalnız ehtiyacı olan ətraf mühitlə qarşılıqlı əlaqəyə girməsini təmin edir.
+
