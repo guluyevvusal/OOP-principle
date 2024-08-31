bb19458901cc7eeb2a4782358c02f1754535216c
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-31 15:56:14 +0400

    Composition created

---
+++ Composition/Composition/Car.cs
@@ -0,0 +1,26 @@
+﻿public class Engine
+{
+    public void Start()
+    {
+        Console.WriteLine("Engine started");
+    }
+}
+
+public class Car
+{
+    private Engine engine;
+
+    public Car()
+    {
+        engine = new Engine();
+    }
+
+    public void Start()
+    {
+        engine.Start();
+        Console.WriteLine("Car started");
+    }
+}
+
+
+// Kompozisiya, siniflər arasında "has-a" əlaqəsi yaratmaq üçün istifadə olunur. Bu, bir sinifin digər siniflərin instansiyalarını özündə saxlamasıdır. Kompozisiya siniflər arasında əlaqələri daha çevik şəkildə qurmağa imkan verir.
